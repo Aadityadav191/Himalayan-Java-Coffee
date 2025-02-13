8c8e1fe518777b3c7cc19aec09c346a32705e482
Aadityadav <aadit.211602@ncit.edu.np>
2025-02-13 15:18:18 +0545

    adding motion

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Hero.css";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
     <>
       <main className="Hero">
-      
-      <h1 >Rise and grind, it's coffee time!</h1>
-        
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+          }}
+        >
+          <h1>Rise and grind, it's coffee time!</h1>
+        </motion.div>
       </main>
     </>
   );
 }
-
-
 
